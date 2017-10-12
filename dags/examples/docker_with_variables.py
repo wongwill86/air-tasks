925897add684f7a1cab41aaf51f7066370a7107c
wongwill86-mv-2 <wongwill86@gmail.com>
2017-10-12 02:52:16 -0400

    Add example dag for docker with variables
    \

---
+++ dags/examples/docker_with_variables.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.custom_plugin import DockerWithVariablesOperator
+
+DAG_ID = 'example_docker_with_variables'
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2017, 5, 1),
+    'cactchup_by_default': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=2),
+    'retry_exponential_backoff': True,
+}
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args=default_args,
+    schedule_interval=None
+)
+
+start = DockerWithVariablesOperator(
+    ['nothing'],
+    task_id='docker_task',
+    command='sh -c "ls /run/variables &&\
+        cat /run/variables/nothing && echo done"',
+    default_args=default_args,
+    image="alpine:latest",
+    dag=dag
+)
