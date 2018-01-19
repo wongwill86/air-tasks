27ed428792d534b454d2531846d2b2c755c79c27
wongwill86-mv-2 <wongwill86@gmail.com>
2018-01-19 03:04:43 -0500

    no longer need entrypoint-test

--- scripts/entrypoint-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source scripts/add-user-docker.sh
-
-# this doens't protect from docker but it's a little more secure
-sudo sed -i "/$AIRFLOW_USER/d" /etc/sudoers
-echo "start script"
-exec sg $DOCKER_GROUP "$*"
