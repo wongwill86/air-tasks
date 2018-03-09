724c5fac3c9b0d11dfe4b326d925d5d4caba2df5
Ran Lu <ranl@princeton.edu>
2018-03-09 11:57:37 -0500

    Print docker stats info to the log
    The code processing the stats dict is based on:
    https://github.com/TomasTomecek/sen/blob/master/sen/util.py

--- plugins/custom/docker_custom.py
+++ plugins/custom/docker_custom.py
@@ -7,6 +7,115 @@ from airflow.models import Variable
 from airflow.operators.docker_operator import DockerOperator
 from airflow.utils.file import TemporaryDirectory
 
+import threading
+
+def _ensure_unicode(s):
+    try:
+        return s.decode("utf-8")
+    except AttributeError:
+        return s
+
+
+def humanize_bytes(bytesize, precision=2):
+    """
+    Humanize byte size figures
+
+    https://gist.github.com/moird/3684595
+    """
+    abbrevs = (
+        (1 << 50, 'PB'),
+        (1 << 40, 'TB'),
+        (1 << 30, 'GB'),
+        (1 << 20, 'MB'),
+        (1 << 10, 'kB'),
+        (1, 'bytes')
+    )
+    if bytesize == 1:
+        return '1 byte'
+    for factor, suffix in abbrevs:
+        if bytesize >= factor:
+            break
+    if factor == 1:
+        precision = 0
+    return '%.*f %s' % (precision, bytesize / float(factor), suffix)
+
+
+# this is taken directly from docker client:
+#   https://github.com/docker/docker/blob/28a7577a029780e4533faf3d057ec9f6c7a10948/api/client/stats.go#L309
+def calculate_cpu_percent(d):
+    cpu_count = len(d["cpu_stats"]["cpu_usage"]["percpu_usage"])
+    cpu_percent = 0.0
+    cpu_delta = float(d["cpu_stats"]["cpu_usage"]["total_usage"]) - \
+                float(d["precpu_stats"]["cpu_usage"]["total_usage"])
+    system_delta = float(d["cpu_stats"]["system_cpu_usage"]) - \
+                   float(d["precpu_stats"]["system_cpu_usage"])
+    if system_delta > 0.0:
+        cpu_percent = cpu_delta / system_delta * 100.0 * cpu_count
+    return cpu_percent
+
+
+# again taken directly from docker:
+#   https://github.com/docker/cli/blob/2bfac7fcdafeafbd2f450abb6d1bb3106e4f3ccb/cli/command/container/stats_helpers.go#L168
+# precpu_stats in 1.13+ is completely broken, doesn't contain any values
+def calculate_cpu_percent2(d, previous_cpu, previous_system):
+    # import json
+    # du = json.dumps(d, indent=2)
+    # logger.debug("XXX: %s", du)
+    cpu_percent = 0.0
+    cpu_total = float(d["cpu_stats"]["cpu_usage"]["total_usage"])
+    cpu_delta = cpu_total - previous_cpu
+    cpu_system = float(d["cpu_stats"]["system_cpu_usage"])
+    system_delta = cpu_system - previous_system
+    online_cpus = d["cpu_stats"].get("online_cpus", len(d["cpu_stats"]["cpu_usage"]["percpu_usage"]))
+    if system_delta > 0.0:
+        cpu_percent = (cpu_delta / system_delta) * online_cpus * 100.0
+    return cpu_percent, cpu_system, cpu_total
+
+def calculate_blkio_bytes(d):
+    """
+
+    :param d:
+    :return: (read_bytes, wrote_bytes), ints
+    """
+    bytes_stats = graceful_chain_get(d, "blkio_stats", "io_service_bytes_recursive")
+    if not bytes_stats:
+        return 0, 0
+    r = 0
+    w = 0
+    for s in bytes_stats:
+        if s["op"] == "Read":
+            r += s["value"]
+        elif s["op"] == "Write":
+            w += s["value"]
+    return r, w
+
+
+def calculate_network_bytes(d):
+    """
+
+    :param d:
+    :return: (received_bytes, transceived_bytes), ints
+    """
+    networks = graceful_chain_get(d, "networks")
+    if not networks:
+        return 0, 0
+    r = 0
+    t = 0
+    for if_name, data in networks.items():
+        r += data["rx_bytes"]
+        t += data["tx_bytes"]
+    return r, t
+
+
+def graceful_chain_get(d, *args):
+    t = d
+    for a in args:
+        try:
+            t = t[a]
+        except (KeyError, ValueError, TypeError):
+            return None
+    return t
+
 
 class DockerConfigurableOperator(DockerOperator):
     """
@@ -25,6 +134,30 @@ class DockerConfigurableOperator(DockerOperator):
         else:
             self.host_args = host_args
         super().__init__(*args, **kwargs)
+
+    def _read_task_stats(self, stream):
+        cpu_total = 0.0
+        cpu_system = 0.0
+        cpu_percent = 0.0
+        for x in stream:
+            blk_read, blk_write = calculate_blkio_bytes(x)
+            net_r, net_w = calculate_network_bytes(x)
+            mem_current = x["memory_stats"]["usage"]
+            mem_total = x["memory_stats"]["limit"]
+
+            try:
+                cpu_percent, cpu_system, cpu_total = calculate_cpu_percent2(x, cpu_total, cpu_system)
+            except KeyError:
+                cpu_percent = calculate_cpu_percent(x)
+
+            self.log.info("cpu: {:.2f}%, mem: {} ({:.2f}%), blk read/write: {}/{}, net rx/tx: {}/{}".
+                          format(cpu_percent,
+                                 humanize_bytes(mem_current),
+                                 (mem_current / mem_total) * 100.0,
+                                 humanize_bytes(blk_read),
+                                 humanize_bytes(blk_write),
+                                 humanize_bytes(net_r),
+                                 humanize_bytes(net_w)))
 
     # This needs to be updated whenever we update to a new version of airflow!
     def execute(self, context):
@@ -80,6 +213,14 @@ class DockerConfigurableOperator(DockerOperator):
             self.container = self.cli.create_container(**container_args)
 
             self.cli.start(self.container['Id'])
+
+            log_reader = threading.Thread(
+                target=self._read_task_stats,
+                args=(self.cli.stats(container=self.container['Id'], decode=True, stream=True),),
+            )
+
+            log_reader.daemon = True
+            log_reader.start()
 
             line = ''
             for line in self.cli.logs(
